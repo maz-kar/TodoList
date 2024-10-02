2174865e0d77c879ce47d213e2494512790734de
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-10-02 14:59:23 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Maziar Layeghkar on 02.10.24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
