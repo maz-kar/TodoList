f7800581a38fd04bb5840a244490a3cc89a233a5
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-10-11 17:13:16 +0200

    Color extended.

---
+++ TodoList/Extension/Color.swift
@@ -0,0 +1,29 @@
+//
+//  Color.swift
+//  TodoList
+//
+//  Created by Maziar Layeghkar on 11.10.24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = Theme()
+    static let launch = Launch()
+}
+
+struct Theme {
+    let background = Color("Background")
+    let accent = Color("AccentColor")
+    let secondaryPurple = Color("SecondaryPurple")
+    let secondaryText = Color("SecondaryText")
+    let textField = Color("TextField")
+
+}
+
+struct Launch {
+    let launchAccent = Color("LaunchAccent")
+    let launchBackground = Color("LaunchBackground")
+}
+
